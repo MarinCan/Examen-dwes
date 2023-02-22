458fbee3aec42c55d42c6dd333e463f315009c0f
Marta M <marmacan.upv@gmail.com>
2023-02-22 11:18:35 +0100

    Formulario y metodo post /add-fotos funcional

--- routes/fotos.js
+++ routes/fotos.js
@@ -4,9 +4,31 @@ const pool = require('../database')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    const resultado = await pool.query('SELECT 1+1')
-    console.log(resultado)
+    // const resultado = await pool.query('SELECT 1+1')
+    // console.log(resultado)
     res.send('fotos')
+});
+
+router.get('/add', function(req, res, next) {
+    res.render('add-fotos')
+});
+
+router.post('/add', async function(req, res, next) {
+    // console.log(req.body)
+    // res.send('ok')
+
+    const { titulo, descripcion, url} = req.body
+    const votos = 0
+
+    await pool.query("INSERT INTO fotos SET ?", {
+        titulo,
+        descripcion,
+        url,
+        votos
+    })
+
+    res.redirect('/fotos')
+    
 });
 
 router.get('/masvotadas', async function(req, res, next) {
